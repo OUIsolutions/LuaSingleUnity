6882a6cf4c66e4b64a2dac68445c5ffcb17eceec
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-04 05:34:29 -0300

    att

--- darwinconf.lua
+++ darwinconf.lua
@@ -2,8 +2,14 @@
 
 function main()
       
-    local out_zip = "lua"..VERSION..".zip"
-    local url= "https://github.com/lua/lua/archive/refs/tags/v"..VERSION..".zip"
+    local version = darwin.argv.get_flag_arg_by_index({"version", "v"}, 1)
+    if version == nil then
+        print("Please provide a version number.")
+        return
+    end
+
+    local out_zip = "lua"..version..".zip"
+    local url= "https://github.com/lua/lua/archive/refs/tags/v"..version..".zip"
     local out_dir = "lua_code"
     if not darwin.dtw.isfile(out_zip) then
         print("Downloading "..out_zip)
