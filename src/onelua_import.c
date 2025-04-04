4ab1c40cafd44eabac2139047e2d660e06679a35
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-04 11:15:44 -0300

    att

--- src/onelua_import.c
+++ src/onelua_import.c
@@ -2,6 +2,11 @@
 #define main LUA_SINGLE_UNITY_MAIN
 #endif
 
+#if defined(LUA_SINGLE_UNITY_POSIX_GUESS)
+    #if defined(__unix__) || defined(__APPLE__) || defined(__linux__)
+        #define LUA_USE_POSIX
+    #endif 
+#endif 
 
 #include "../lua/onelua.c"
 #if defined(LUA_SINGLE_UNITY_IMPLEMENT_MATH)
