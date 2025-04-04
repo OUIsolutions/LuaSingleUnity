0e8e6b01ab97bf5dd0a2f69c7c40b4a3e47c8e1c
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-04 10:25:15 -0300

    att

---
+++ src/math.c
@@ -0,0 +1,60 @@
+
+double pow(double x, double y) {
+    return pow(x, y);
+}
+
+double floor(double x) {
+    return floor(x);
+}
+
+double fmod(double x, double y) {
+    return fmod(x, y);
+}
+
+double sin(double x) {
+    return sin(x);
+}
+
+double cos(double x) {
+    return cos(x);
+}
+
+double tan(double x) {
+    return tan(x);
+}
+
+double asin(double x) {
+    return asin(x);
+}
+
+double acos(double x) {
+    return acos(x);
+}
+
+double atan2(double y, double x) {
+    return atan2(y, x);
+}
+
+double ceil(double x) {
+    return ceil(x);
+}
+
+double sqrt(double x) {
+    return sqrt(x);
+}
+
+double log(double x) {
+    return log(x);
+}
+
+double log2(double x) {
+    return log2(x);
+}
+
+double log10(double x) {
+    return log10(x);
+}
+
+double exp(double x) {
+    return exp(x);
+}
--- src/onelua_import.c
+++ src/onelua_import.c
@@ -2,6 +2,9 @@
 #define main LUA_SINGLE_UNITY_MAIN
 #endif
 #include "../lua/onelua.c"
+#ifdef LUA_SINGLE_UNITY_IMPLEMENT_MATH 
+#include "math.c"
+#endif 
 
 #ifdef LUA_SINGLE_UNITY_HIDE_MAIN
 #undef main
