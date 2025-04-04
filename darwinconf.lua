a73c5d9e26bfac3a49c51d9489e84e505e8e460e
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-04 05:37:46 -0300

    att

--- darwinconf.lua
+++ darwinconf.lua
@@ -1,24 +1,29 @@
 
 
-function main()
-      
-    local version = darwin.argv.get_flag_arg_by_index({"version", "v"}, 1)
-    if version == nil then
-        print("Please provide a version number.")
-        return
-    end
+OUT_ZIP = "lua.zip"
+OUT_DIR = "lua"
 
-    local out_zip = "lua"..version..".zip"
-    local url= "https://github.com/lua/lua/archive/refs/tags/v"..version..".zip"
-    local out_dir = "lua_code"
-    if not darwin.dtw.isfile(out_zip) then
-        print("Downloading "..out_zip)
-        os.execute("curl -L "..url.." -o "..out_zip)
-    end
 
-    if not darwin.dtw.isdir(out_dir) then
-        print("Unzipping "..out_zip)
-        os.execute("unzip -q "..out_zip)
-    end
-     
-end 
+local version = darwin.argv.get_flag_arg_by_index({"version", "v"}, 1)
+if version == nil then
+    print("Please provide a version number.")
+    return
+end
+
+print("Lua Single Unity Build")
+print("===================================================================")
+
+
+local url= "https://github.com/lua/lua/archive/refs/tags/v"..version..".zip"
+
+
+if not darwin.dtw.isfile(OUT_ZIP) then
+    print("Downloading "..OUT_ZIP)
+    os.execute("curl -L "..url.." -o "..OUT_ZIP)
+end
+
+if not darwin.dtw.isdir(OUT_DIR) then
+    print("Unzipping "..OUT_ZIP)
+    os.execute("unzip -q "..OUT_ZIP)
+end
+    
