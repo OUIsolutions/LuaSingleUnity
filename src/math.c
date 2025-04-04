acf7193e8a1af434636ba4ca9693935c4da86044
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-04 10:32:38 -0300

    att

--- src/math.c
+++ src/math.c
@@ -1,29 +1,133 @@
+double pow(double x, double y) {
+    double result = 1.0;
+    for (int i = 0; i < y; i++) {
+        result *= x;
+    }
+    return result;
+}
 
-double pow(double x, double y);
+double floor(double x) {
+    int result = (int)x;
+    if (x < 0 && x != result) {
+        result--;
+    }
+    return result;
+}
 
-double floor(double x);
+double fmod(double x, double y) {
+    return x - y * floor(x / y);
+}
 
-double fmod(double x, double y);
+double sin(double x) {
+    // Taylor series approximation
+    double result = x;
+    double term = x;
+    for (int i = 1; i <= 10; i++) {
+        term *= -x * x / ((2 * i) * (2 * i + 1));
+        result += term;
+    }
+    return result;
+}
 
-double sin(double x);
+double cos(double x) {
+    // Taylor series approximation
+    double result = 1.0;
+    double term = 1.0;
+    for (int i = 1; i <= 10; i++) {
+        term *= -x * x / ((2 * i - 1) * (2 * i));
+        result += term;
+    }
+    return result;
+}
 
-double cos(double x);
+double tan(double x) {
+    return sin(x) / cos(x);
+}
 
-double tan(double x);
+double asin(double x) {
+    // Taylor series approximation
+    double result = x;
+    double term = x;
+    for (int i = 1; i <= 10; i++) {
+        term *= (2 * i - 1) * (2 * i - 1) * x * x / (2 * i * (2 * i + 1));
+        result += term;
+    }
+    return result;
+}
 
-double asin(double x);
+double acos(double x) {
+    return M_PI / 2 - asin(x);
+}
 
-double acos(double x);
+double atan2(double y, double x) {
+    if (x > 0) {
+        return atan(y / x);
+    } else if (x < 0 && y >= 0) {
+        return atan(y / x) + M_PI;
+    } else if (x < 0 && y < 0) {
+        return atan(y / x) - M_PI;
+    } else if (x == 0 && y > 0) {
+        return M_PI / 2;
+    } else if (x == 0 && y < 0) {
+        return -M_PI / 2;
+    } else {
+        return 0;
+    }
+}
 
-double atan2(double y, double x);
+double ceil(double x) {
+    int result = (int)x;
+    if (x > result) {
+        result++;
+    }
+    return result;
+}
 
-double ceil(double x);
+double sqrt(double x) {
+    double result = x;
+    for (int i = 0; i < 10; i++) {
+        result = 0.5 * (result + x / result);
+    }
+    return result;
+}
 
-double sqrt(double x);
+double log(double x) {
+    // Taylor series approximation
+    double result = 0.0;
+    double term = (x - 1) / x;
+    for (int i = 1; i <= 10; i++) {
+        result += term / i;
+        term *= (x - 1) / x;
+    }
+    return result;
+}
 
-double log(double x);
+double log2(double x) {
+    return log(x) / log(2);
+}
 
-double log2(double x);
+double log10(double x) {
+    return log(x) / log(10);
+}
 
-double log10(double x);
-double exp(double x);
+double exp(double x) {
+    // Taylor series approximation
+    double result = 1.0;
+    double term = 1.0;
+    for (int i = 1; i <= 10; i++) {
+        term *= x / i;
+        result += term;
+    }
+    return result;
+}
+
+double atan(double x) {
+    // Taylor series approximation
+    double result = x;
+    double term = x;
+    for (int i = 1; i <= 10; i++) {
+        term *= -x * x * (2 * i - 1) / (2 * i + 1);
+        result += term;
+    }
+    return result;
+}
