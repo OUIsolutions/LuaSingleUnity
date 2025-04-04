e5b05dd17a6e8e8855af41ee4591411509fb2e16
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-04 11:12:30 -0300

    att

--- src/onelua_import.c
+++ src/onelua_import.c
@@ -1,11 +1,13 @@
-#ifdef LUA_SINGLE_UNITY_HIDE_MAIN
+#if defined(LUA_SINGLE_UNITY_HIDE_MAIN)
 #define main LUA_SINGLE_UNITY_MAIN
 #endif
+
+
 #include "../lua/onelua.c"
-#ifdef LUA_SINGLE_UNITY_IMPLEMENT_MATH 
+#if defined(LUA_SINGLE_UNITY_IMPLEMENT_MATH)
 #include "math.c"
 #endif 
 
-#ifdef LUA_SINGLE_UNITY_HIDE_MAIN
+#if defined(LUA_SINGLE_UNITY_HIDE_MAIN)
 #undef main
 #endif
