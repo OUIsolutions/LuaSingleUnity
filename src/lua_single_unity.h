fb49a70ab713ea7ad2489163aaa5f0aba613389e
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-04 21:06:40 -0300

    att

--- src/lua_single_unity.h
+++ src/lua_single_unity.h
@@ -1,4 +1,13 @@
 #include "guess_os.h"
+#include "../lua/lprefix.h"
+
+/* setup for luaconf.h */
+#define LUA_CORE
+#define LUA_LIB
+#define ltable_c
+#define lvm_c
+
+
 
 #include "../lua/lua.h"
 #include "../lua/lualib.h"
