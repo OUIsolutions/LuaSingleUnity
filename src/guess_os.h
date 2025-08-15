e8a1febe55ea5c4d4b3c5079b96f55cd577c3306
Jose Rafael Camejo <jose20camejo@gmail.com>
2025-08-15 13:29:05 -0300

    recognize mac

--- src/guess_os.h
+++ src/guess_os.h
@@ -1,14 +1,12 @@
 
-
-
 #if defined(LUA_SINGLE_UNITY_GUESS_OS)
 
     #if defined(__APPLE__)
         #include <TargetConditionals.h>
-        #if defined(TARGET_OS_IPHONE) || defined(TARGET_IPHONE_SIMULATOR)
-            #define LUA_USE_IOS
-        #elif defined(__MACH__)
+        #if TARGET_OS_MAC && !TARGET_OS_IPHONE
             #define LUA_USE_MACOSX
+        #elif TARGET_OS_IPHONE || TARGET_OS_SIMULATOR
+            #define LUA_USE_IOS
         #endif
     #elif defined(__linux__)
         #define LUA_USE_LINUX
@@ -16,6 +14,6 @@
         #define LUA_USE_WINDOWS
     #elif defined(__unix__) || defined(__FreeBSD__) || defined(__NetBSD__)
         #define LUA_USE_POSIX
-    #endif 
+    #endif
 
 #endif
